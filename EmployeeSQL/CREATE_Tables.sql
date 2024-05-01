e0c7eaf1b884ee827c6fdb66f155646236af9856
Soumya <soumya202@gmail.com>
2024-05-01 14:29:39 -0400

    corrections to table column sizes

--- EmployeeSQL/CREATE_Tables.sql
+++ EmployeeSQL/CREATE_Tables.sql
@@ -1,20 +1,20 @@
 CREATE TABLE Titles (
-  title_id VARCHAR PRIMARY KEY,
-  title VARCHAR NOT NULL
+  title_id VARCHAR(5) PRIMARY KEY,
+  title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Departments (
-  dept_no VARCHAR PRIMARY KEY,
-  dept_name VARCHAR NOT NULL
+  dept_no VARCHAR(4) PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Employees (
   emp_no INT PRIMARY KEY,
-  emp_title_id VARCHAR NOT NULL,
+  emp_title_id VARCHAR(5) NOT NULL,
   birth_date DATE NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL,
-  sex VARCHAR NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  sex VARCHAR(1) NOT NULL,
   hire_date DATE NOT NULL,
   FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
 );
@@ -26,7 +26,7 @@ CREATE TABLE Salaries (
 );
 
 CREATE TABLE Dept_Manager (
-  dept_no VARCHAR NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
   emp_no INT NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
@@ -34,7 +34,7 @@ CREATE TABLE Dept_Manager (
 
 CREATE TABLE Dept_Emp (
   emp_no INT NOT NULL,
-  dept_no VARCHAR NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
   FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
 );
